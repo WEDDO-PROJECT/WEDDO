d4ada5c794145ad2c8992e57047773c10f80c1c8
SlimGuenidez <leagueofslim@outlook.com>
2022-06-06 01:26:25 +0200

    pullin

--- src/constants/BasePath.js
+++ src/constants/BasePath.js
@@ -3,5 +3,5 @@
 
 
 // export default "http://localhost:3000";
-export default "http://192.168.130.27:3000";
-// export default "http://192.168.1.3:3000"
+// export default "http://192.168.130.27:3000";
+export default "http://192.168.1.3:3000"
